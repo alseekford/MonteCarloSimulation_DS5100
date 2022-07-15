af426140751caf79d6d98efc4078cd928cce1d84
Anne Louise Seekford <71660299+alseekford@users.noreply.github.com>
2022-07-15 13:30:26 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='MonteCarlo_DS5100',
+      version='1.0',
+      description='Implements a Monte Carlo Simulator. Contains three classes: Die, Game, and Analyzer in montecarlo.py',
+      url='https://github.com/alseekford/MonteCarloSimulation_DS5100',
+      author='Anne Louise Seekford',
+      author_email='bng3be@virginia.edu',
+      license='MIT',
+      packages=['montecarlo_simulator'])
